4bbe3413a24bb0cf220829c917018330b44c613e
insukL <inkcloud365@gmail.com>
2023-11-06 05:21:17 +0900

    feat: add csv writer

--- src/main/java/com/thisisaniceteam/batch/tasklet/DailyRegisteredFileTasklet.java
+++ src/main/java/com/thisisaniceteam/batch/tasklet/DailyRegisteredFileTasklet.java
@@ -1,31 +1,36 @@
 package com.thisisaniceteam.batch.tasklet;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Map;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@StepScope
 @Component
 public class DailyRegisteredFileTasklet implements Tasklet {
+
+    @Value("#{jobParameters[date]}")
+    private String date;
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 
         Map<String, Object> context = chunkContext.getStepContext().getJobExecutionContext();
 
-        System.out.println(context.keySet());
+        File file = new File("C:/projects/this-is-a-nice-backend-batch/src/main/resources/static/daily_registered_count.csv");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
 
-        if (context.containsKey("count")) {
-            // File Write
-            System.out.println((int) context.get("count"));
-        }
-
-        else {
-            // Zero Value Write
-            System.out.println("failed");
-        }
+        bw.write(date + "," + context.get("count"));
+        bw.newLine();
+        bw.flush();
+        bw.close();
 
         return RepeatStatus.FINISHED;
     }
