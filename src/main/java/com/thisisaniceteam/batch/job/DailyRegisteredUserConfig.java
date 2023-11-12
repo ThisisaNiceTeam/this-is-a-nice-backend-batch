13e017a8825c4e74ec7081cc9e0e376792280ae9
insukL <inkcloud365@gmail.com>
2023-11-12 18:39:55 +0900

    style: fix code format

--- src/main/java/com/thisisaniceteam/batch/job/DailyRegisteredUserConfig.java
+++ src/main/java/com/thisisaniceteam/batch/job/DailyRegisteredUserConfig.java
@@ -1,20 +1,15 @@
 package com.thisisaniceteam.batch.job;
 
 import com.thisisaniceteam.batch.item.writer.CountItemWriter;
-import com.thisisaniceteam.batch.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecutionListener;
-import org.springframework.batch.core.StepListener;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -43,7 +38,7 @@ public class DailyRegisteredUserConfig {
             CountItemWriter itemWriter) {
 
         return new StepBuilder("dailyRegisteredUserStep", jobRepository)
-                .<String, String> chunk(10, transactionManager)
+                .<String, String>chunk(10, transactionManager)
                 .reader(itemReader)
                 .writer(itemWriter)
                 .build();
