fd3e0ceec8c88ecedfa8cb4e6d2102f1ea068e6f
insukL <inkcloud365@gmail.com>
2023-12-18 16:56:34 +0900

    feat: add weekly activate user config

---
+++ src/main/java/com/thisisaniceteam/batch/job/WeeklyActivateUserConfig.java
@@ -0,0 +1,12 @@
+package com.thisisaniceteam.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class WeeklyActivateUserConfig {
+
+    
+
+}
